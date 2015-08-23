68b556eb9bf543d289fa97b664d02d47f0cd98ed
Mike Bland <michael.bland@gsa.gov>
2015-08-23 17:19:35 -0400

    Improve ModuleTest structure
    Using `extend GoScript` in the `setup` method is more analogous to how the
    `./go` script is built. Also, we now properly create and destroy a
    CommandGroup for each test, but a forthcoming commit will make the
    CommandGroup easier to use and prevent duplicate definitions.

--- _test/go_test.rb
+++ _test/go_test.rb
@@ -8,10 +8,15 @@ require 'stringio'
 
 module GoScript
   class ModuleTest < ::Minitest::Test
-    include GoScript
+    attr_accessor :command_group
 
-    def self.command_group
-      @command_group ||= CommandGroup.add_group 'Test commands'
+    def setup
+      extend GoScript
+      @command_group = CommandGroup.add_group 'Test commands'
+    end
+
+    def teardown
+      CommandGroup.groups.pop
     end
 
     def test_exec_cmd
@@ -26,7 +31,7 @@ module GoScript
 
     def test_def_command
       result = nil
-      def_command :test_cmd, ModuleTest.command_group, 'Test command' do
+      def_command :test_cmd, command_group, 'Test command' do
         result = 'success'
       end
       test_cmd
@@ -35,38 +40,35 @@ module GoScript
 
     def test_invoke_command_with_optional_argument
       result = nil
-      def_command(:test_cmd2, ModuleTest.command_group,
-        'Second test command') do |optional_argv = []|
+      def_command(:test_cmd, command_group,
+        'Test command') do |optional_argv = []|
         result = 'success ' + optional_argv.join(' ')
       end
-      test_cmd2 %w(foo bar)
+      test_cmd %w(foo bar)
       assert_equal 'success foo bar', result
     end
 
     def test_invoke_command_without_optional_argument
       result = nil
-      def_command(:test_cmd3, ModuleTest.command_group,
-        'Third test command') do |optional_argv = []|
+      def_command(:test_cmd, command_group,
+        'Test command') do |optional_argv = []|
         result = 'success ' + optional_argv.join(' ')
       end
-      test_cmd3
+      test_cmd
       assert_equal 'success ', result
     end
 
     def test_execute_command
       result = nil
-      def_command(:test_cmd4, ModuleTest.command_group,
-        'Fourth test command') do |moar, args|
+      def_command :test_cmd, command_group, 'Test command' do |moar, args|
         result = [moar, args]
       end
-      execute_command %w(test_cmd4 moar args)
+      execute_command %w(test_cmd moar args)
       assert_equal %w(moar args), result
     end
 
     def test_execute_command_fail_with_usage_message_if_command_is_nil
       orig_stderr, $stderr = $stderr, StringIO.new
-      # Ensure the 'Test commands' group exists
-      ModuleTest.command_group
       exception = assert_raises(SystemExit) { execute_command [] }
       assert_equal 1, exception.status
       assert $stderr.string.start_with? "Usage: #{$PROGRAM_NAME}"
@@ -77,8 +79,6 @@ module GoScript
 
     def test_execute_command_show_usage_message_when_help_option_specified
       orig_stdout, $stdout = $stdout, StringIO.new
-      # Ensure the 'Test commands' group exists
-      ModuleTest.command_group
       exception = assert_raises(SystemExit) { execute_command %w(-h) }
       assert_equal 0, exception.status
       assert $stdout.string.start_with? "Usage: #{$PROGRAM_NAME}"
